381c08ae8ebed97bc9938c663f195b847f9ba956
root <tsegabirhanugma>
2022-03-14 14:05:29 -0700

    comb3 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = i + 1 ; j <=9 ; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 9 && j < 10)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
