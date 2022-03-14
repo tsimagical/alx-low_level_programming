b85f31cf7c88251adf07117ea604c2ae951e2f0c
root <tsegabirhanugma>
2022-03-14 14:09:38 -0700

    fix: comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,12 @@ int main(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = i + 1 ; j <=9 ; j++)
+		for (j = i + 1 ; j <= 9 ; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (i < 9 && j < 10)
+			
+			if (i < 8 && j < 9)
 			{
 				putchar(',');
 				putchar(' ');
