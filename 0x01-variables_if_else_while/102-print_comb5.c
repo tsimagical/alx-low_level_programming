1299d2074bb5a3c08edf92dc8616008b29c4551a
root <tsegabirhanugma>
2022-03-14 14:48:06 -0700

    fix: comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,12 @@ int main(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = i + 1 ; j <= 9 ; j++)
+		for (j = 0 ; j <= 9 ; j++)
 		{
 			for (k = 0 ; k  <= 9 ; k++)
 			{
-				for (l = k + 1 ; l <= 9 ; l++)
-				{
-					
+				for (l = 0 ; l <= 9 ; l++)
+				{					
 					putchar(i + '0');
 					putchar(j + '0');
 					if (j < 9)
