5e851d98ee511e46591fe455b1a9cb2132cbd7d8
root <tsegabirhanugma>
2022-03-14 13:00:25 -0700

    print number done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+		printf(n);
+	printf("\n");
+
+	return (0);
+}
