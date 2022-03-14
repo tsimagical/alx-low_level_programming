089f838048f30ebda9e68afbf114ffeaa172b21a
root <tsegabirhanugma>
2022-03-14 13:48:02 -0700

    fix: comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,8 +14,11 @@ int main(void)
 	for (ch = 0 ; ch <= 9 ; ch++)
 	{
 		putchar(ch + '0');
-		putchar(',');
-		putchar(' ');
+		if (ch < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
