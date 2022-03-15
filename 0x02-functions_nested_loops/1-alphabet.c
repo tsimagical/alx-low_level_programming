92761b58078fe6ddcd6cd414b94af9ce9b443360
melake <wubshetmelake@gmail.com>
2022-03-15 14:38:04 +0300

    hotfix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  *
  * Return: empty
  */
-void print_alphabet(void) 
+void print_alphabet(void)
 {
     char ch;
-
-	for (ch = 'a' ; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
+	
+    for (ch = 'a' ; ch <= 'z'; ch++)
+        _putchar(ch);
+    _putchar('\n');
 }
 
