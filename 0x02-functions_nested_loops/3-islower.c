4860aede68e865fabd92ba608cafc3d3b1504491
root <tsegabirhanugma>
2022-03-15 05:41:48 -0700

    hotfix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,14 @@
 
 /**
  * is_lower - check lower case
- *
+ * Description: check the given character is lower case or not
+ * Parameter: C characther
  * Return: 1 if it is lower and 0 otherwise
  */
 int _islower(int c)
 {
 	if (islower(c))
 		return (1);
-	return (0);
-			
+	return (0);			
 }
 
