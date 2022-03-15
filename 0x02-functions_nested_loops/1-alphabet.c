5edb0da46abdc741a01452689bdae44aaf632a9c
melake <wubshetmelake@gmail.com>
2022-03-15 14:32:18 +0300

    alphabet function done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+    return (0);
+}
+
+void print_alphabet(void) {
+    char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
+
