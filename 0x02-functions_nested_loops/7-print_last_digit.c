bb5219dcbe60712a5b6909fbaa6fbb4c2f9276fb
tsimagical <99464999+tsimagical@users.noreply.github.com>
2022-03-15 16:41:24 +0300

    hotfix: print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,19 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n % 10;
+	last_digit = _abs(n % 10);
 	_putchar(last_digit + '0');
 	return (last_digit);
+}
+
+/**
+ * _abs - absolute value of a number
+ * @n: a number
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
 }
