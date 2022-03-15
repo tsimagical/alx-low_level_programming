492bc9be77783e86201b0ac16d8b1efd5f3c4d94
root <tsegabirhanugma>
2022-03-15 05:16:33 -0700

    alphabetx10 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print all alphabet in small case 10 times
+ *
+ * Return: empty
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+		print_alphabet();
+}
+
