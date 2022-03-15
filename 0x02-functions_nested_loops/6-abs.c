d008d14cec72383f5913cfadc1615221525d09a7
root <tsegabirhanugma>
2022-03-15 06:29:24 -0700

    abs done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of a number
+ * @n: a number
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
+
