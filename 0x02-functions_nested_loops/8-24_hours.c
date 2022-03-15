8a6eec6004c94ff9a29c46e00a8fa621a1256227
tsimagical <99464999+tsimagical@users.noreply.github.com>
2022-03-15 22:56:44 +0300

    jack bauer done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* jack_bauer - print every miniute of the day
+* 
+*/
+void jack_bauer(void)
+{
+	int t,x;
+  
+	for (t = 0 ; t < 24 ; t++)
+	{
+		for (x = 0 ; x < 60 ; x++)
+		{
+			_putchar(t / 10 + '0');
+			_putchar(t % 10 + '0');
+			_putchar(':');
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
