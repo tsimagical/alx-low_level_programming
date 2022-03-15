a9853142bbca945a6c2c8293447175e9137df8e2
root <tsegabirhanugma>
2022-03-15 13:00:53 -0700

    hotfix: 24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
 * jack_bauer - print every miniute of the day
-* 
+* Description: print jack beauer 
 */
 void jack_bauer(void)
 {
-	int t,x;
-  
+	int t, x;
+	
 	for (t = 0 ; t < 24 ; t++)
 	{
 		for (x = 0 ; x < 60 ; x++)
