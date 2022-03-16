a5a2be6cc442a36daeb4f8efc4388f590b715c19
root <tsegabirhanugma>
2022-03-16 01:30:07 -0700

    print to 98 done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_to_98 - prints the numbers to 98
+ */
+void print_to_98(int n)
+{
+	int i;
+	
+	if (n > 98)
+		i = -1;
+	else
+		i = 1;
+	for (y = n ; y <= 98 ; y = y + i)
+	{
+		if (y >= 10)
+		{
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+		else
+		{
+			_putchar(y + '0');
+		}
+		if (y != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}	
+	_putchar('\n');
+	
+}
