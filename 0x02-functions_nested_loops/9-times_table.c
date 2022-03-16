aa83c4e6268349ec59c50b9319f2b40a05501d3a
root <tsegabirhanugma>
2022-03-16 00:52:08 -0700

    hotfix: 9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,8 +12,19 @@ void times_table(void)
 	{
 		for (y = 0 ; y < 10 ; y++)
 		{
-			_putchar((x * y) + '0');
-			
+			int z;
+
+			z = x * y;
+
+			if( z >= 10)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
 			if (y < 9)
 			{
 				_putchar(',');
