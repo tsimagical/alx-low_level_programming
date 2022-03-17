c52e02f793b53e2b33701f6e4a55297bbb8c02ae
tsimagical <99464999+tsimagical@users.noreply.github.com>
2022-03-17 22:46:58 +0300

    update main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -12,3 +12,13 @@ int main(void)
 
 	return (0);
 }
+
+void positive_or_negative(i)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+}
