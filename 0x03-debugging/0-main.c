4b96fe2350763c308593f9d2dbbbddca2b8d3165
tsimagical <99464999+tsimagical@users.noreply.github.com>
2022-03-17 22:50:34 +0300

    update main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -12,14 +13,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-}
-
-void positive_or_negative(i)
-{
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
 }
